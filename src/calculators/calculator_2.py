97e20af8ab8c20c7f6310a2daf713a4ef57932bd
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-03-04 17:08:30 -0300

    Aplicando Tratamento de Erros no Projeto

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,9 +1,12 @@
 from typing import Dict, List
 from flask import request as FlaskRequest
-from src.drivers.numpy_handler import NumpyHandler
-
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
 
 class Calculator2:
+    def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+        self.__driver_handler = driver_handler
+
     def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
@@ -14,15 +17,14 @@ class Calculator2:
 
     def __validate_body(self, body: Dict) -> List[float]:
         if "numbers" not in body:
-            raise Exception("body mal formatado!")
+            raise HttpUnprocessableEntityError("body mal formatado!")
 
         input_data = body["numbers"]
         return input_data
 
     def __process_data(self, input_data: List[float]) -> float:
-        numpy_handler = NumpyHandler()
         first_process_result = [(num * 11) ** 0.95 for num in input_data]
-        result = numpy_handler.standaar_derivation(first_process_result)
+        result = self.__driver_handler.standard_derivation(first_process_result)
         return 1/result
 
     def __format_response(self, calculated_number: float) -> Dict:
